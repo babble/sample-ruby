874d1e5822d06c71c627e7931b5de47e54c879f8
Jim Menard <jim@10gen.com>
2008-09-09 09:53:02 -0400

    Code clean-up.

--- controller.rb
+++ controller.rb
@@ -1,16 +1,14 @@
+require 'model'
 
-require( "model" );
+puts "name: <b>#{ $request.name }</b><br>"
 
-puts( "name: <b>#{ $request.name }</b><br>" );
-
-m = MyModel.find( $request.name );
-if ( m )
+m = MyModel.find($request.name)
+if m
   m.inc
 else
-  m = MyModel.new( $request.name );
+  m = MyModel.new($request.name)
 end
-$db.ms.save( m );
+$db.ms.save(m)
 
-puts( "name: <b>#{ m.name }</b><br>" );
-puts( "count: <b>#{ m.counter }</b><br>" );
-
+puts "name: <b>#{ m.name }</b><br>"
+puts "count: <b>#{ m.counter }</b><br>"
--- model.rb
+++ model.rb
@@ -1,24 +1,23 @@
+class MyModel
 
-class MyModel 
+  attr_accessor :counter, :name
 
-  attr_accessor :counter
-  attr_accessor :name
-
-  def initialize( name=nil )
+  def initialize(name=nil)
     @name = name
-    @counter = 1;
+    @counter = 1
   end
-  
+
   def inc
     @counter += 1
   end
 
-  def self.find( name )
-    temp = Struct.new( :name )
-    t = temp.new( name );
-    return $db.ms.findOne( t );
+  def self.find(name)
+    t = Struct.new(:name).new(name)
+    # This also works:
+    # t = {:name => name}
+    return $db.ms.findOne(t)
   end
 
 end
 
-$db.ms.setConstructor( MyModel );
+$db.ms.setConstructor(MyModel)
