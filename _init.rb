8f4d5cb0c8c1e3de59aed8423f2331f5aed00f84
Jim Menard <jim@10gen.com>
2008-09-09 10:36:34 -0400

    More readable syntax and a better comment.

--- _init.rb
+++ _init.rb
@@ -1,11 +1,17 @@
 # Note that we can use Ruby for the _init file. However, instead of just
 # defining a method named mapUrlToJxpFile, we need to create a Proc and store
-# it in the 'mapUrlToJxpFile' slot of the global scope object.
+# it in the 'mapUrlToJxpFile' slot of the global scope.
 #
-# The syntax $scope.mapUrlToJxpFile is the same as $scope['mapUrlToJxpFile'].
-# The JSObject class creates accessor methods on the fly.
+# $mapUrlToJxpFile is a JSFunction object. We can simply assign a new value to
+# it which is a Proc. There is more than one way to write this:
+#
+#   $mapUrlToJxpFile = Proc.new ...
+#   $scope['mapUrlToJxpFile'] = Proc.new ...
+#   $scope.mapUrlToJxpFile = Proc.new ...
+#
+# The first method works because all $scope slots are turned into globals.
 
-$scope.mapUrlToJxpFile = Proc.new { |uri, req|
+$mapUrlToJxpFile = Proc.new { |uri, req|
   if uri =~ /^.(\w+)$/
     req['name'] = $1
     "/controller.rb"
